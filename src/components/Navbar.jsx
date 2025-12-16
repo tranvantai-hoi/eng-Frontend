e4966ebe1bd1df4bfffa26de73ed70ddfb72fe3d
tranvantai-hoi <tranvantai@ttn.edu.vn>
2025-12-16 13:23:54 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = ({ isLoggedIn, onLogout, role, fullname }) => {
                   </div>
                   
                   {isAdmin && (
-                    <Link to="/admin/dashboard" className="flex items-center gap-3 rounded-lg px-3 py-2 text-sm text-slate-600 hover:bg-blue-50 hover:text-blue-700">
+                    <Link to="/admin/user" className="flex items-center gap-3 rounded-lg px-3 py-2 text-sm text-slate-600 hover:bg-blue-50 hover:text-blue-700">
                       <div className="flex h-8 w-8 items-center justify-center rounded-full bg-blue-100 text-blue-600">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-4"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" /></svg>
                       </div>
